89a46a288ed21b34ec81dcce2b233644b1fcbef9
Davith Shamiryan <dshamiryan@list.ru>
2021-11-11 19:12:44 +0400

    Add project files.

---
+++ EnumerableApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 25, 26, 85, 96, 12, 1, 5, 8 };
+            int value = numbers._FirstOrDefault(x => x > 26);
+            Console.WriteLine(value);
+            int lastvalue = numbers._LastOrDefault(x => x < 7);
+            Console.WriteLine(lastvalue);
+            bool anyvalue = numbers._Any(x => x > 97);
+            Console.WriteLine(anyvalue);
+            int count = numbers._Count(x => x < 100);
+            Console.WriteLine(count);
+            bool allcount = numbers._All(x => x < 100);
+            Console.WriteLine(allcount);
+            int sum = numbers._Sum(x => x);
+            Console.WriteLine(sum);
+            int min = numbers._Min(x => x);
+            Console.WriteLine(min);
+            int max = numbers._Max(x => x);
+            Console.WriteLine(max);
+            double average = numbers._Average(x => x);
+            Console.WriteLine(average);
+        }
+    }
+}
---
+++ EnumerableApp/_Enumerable.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Build.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerableApp
+{
+    public static class _Enumerable
+    {
+        public static TSource _FirstOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+
+            foreach (TSource element in source)
+            {
+                if (predicate(element)) return element;
+            }
+            return default(TSource);
+        }
+        public static TSource _LastOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+
+            TSource result = default(TSource);
+            foreach (TSource element in source)
+            {
+                if (predicate(element))
+                {
+                    result = element;
+                }
+            }
+            return result;
+        }
+        public static bool _Any<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (TSource element in source)
+            {
+                if (predicate(element)) return true;
+            }
+            return false;
+        }
+        public static int _Count<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            int count = 0;
+            foreach (TSource element in source)
+            {
+                checked
+                {
+                    if (predicate(element)) count++;
+                }
+            }
+            return count;
+        }
+        public static bool _All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+
+            foreach (TSource element in source)
+            {
+                if (!predicate(element)) return false;
+            }
+            return true;
+        }
+        public static int _Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector)
+        {
+            return Enumerable.Sum(Enumerable.Select(source, selector));
+        }
+        public static IEnumerable<TResult> _Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, TResult> selector)
+        {
+            
+            return SelectIterator<TSource, TResult>(source, selector);
+        }
+
+        static IEnumerable<TResult> SelectIterator<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, int, TResult> selector)
+        {
+            int index = -1;
+            foreach (TSource element in source)
+            {
+                checked { index++; }
+                yield return selector(element, index);
+            }
+        }
+        public static int _Min<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector)
+        {
+            return Enumerable.Min(Enumerable.Select(source, selector));
+        }
+        public static int _Max<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector)
+        {
+            return Enumerable.Max(Enumerable.Select(source, selector));
+        }
+        public static double _Average<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector)
+        {
+            return Enumerable.Average(Enumerable.Select(source, selector));
+        }
+    }
+}
